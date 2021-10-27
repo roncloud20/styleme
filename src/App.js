4778d3403b2bef1596c727204f84c88b1bd2d9b4
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2021-10-27 23:37:29 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import nifemi from './nifemi.jpeg';  
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>
+          Style Me
+        </div>
+        <div>
+          <input type="text" placeholder="Search for styles" />
+        </div>
+        <nav>
+          <li><a href="#women" placeholder="women">women</a></li>
+          <li><a href="#men" placeholder="men">men</a></li>
+          <li><a href="#babies" placeholder="babies">babies</a></li>
+          <li><a href="#teens" placeholder="teens">teens</a></li>
+          <li><a href="#senior-citizens" placeholder="senior citizen">senior citizen</a></li>
+          <li><a href="#couples" placeholder="couples">couples</a></li>
+          <li><a href="#families" placeholder="families">families</a></li>
+          <li><a href="#asoebi" placeholder="asoebi">asoebi</a></li>
+        </nav>
       </header>
+      <main>
+        <div className="Card" style={{ backgroundImage:`url(${nifemi})` }}>
+          <li className="Likes"><span>&hearts;</span> 888</li>
+          <li className="Add"><button>+ Use</button> </li>
+          <li className="Style-name">Style name</li>
+        </div>
+      </main>
     </div>
   );
 }
