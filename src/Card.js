b9c4e6bc7ebd74babceb85dfe92d35a895c421ca
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2021-10-28 22:34:01 +0100

    Refactoring the props in the card component

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,7 @@ import React from 'react';
 // import nifemi from './nifemi.jpeg';  
 import 'tachyons';
 
-const Card = (props) => {
-  const {id, styleName, likes, imageUrl} = props;
+const Card = ({id, styleName, likes, imageUrl}) => {
     return (
         <div className="Card grow pa3 bw3 shadow-5" key={id} style={{ backgroundImage:`url(${imageUrl})` }}>
           <li className="Likes"><span>&hearts;</span> {likes}</li>
